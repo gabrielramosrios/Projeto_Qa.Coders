7a91c90dbacd2e741a38b5e06a9d7e2568bc1608
Gabriel Ramos Rios <106835470+gabrielramosrios@users.noreply.github.com>
2023-05-12 14:28:05 -0300

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -7,14 +7,24 @@ require_relative 'helper.rb'
 require_relative 'page_helper.rb'
 
 ENVIRONMENT = ENV['ENVIRONMENT']
-
 puts "ENVIRONMENT is runner >>> #{ENVIRONMENT}"
-
 CONFIG = YAML.load_file(File.dirname(__FILE__) + "/environments/#{ENVIRONMENT}.yaml")
 World(Pages, Helper)
 
+
 Capybara.configure do |config|
-    config.default_driver = :selenium_chrome
+    case ENV['BROWSER']
+    when 'chrome_headless'
+      config.default_driver = :selenium_chrome_headless
+    when 'chrome'
+      config.default_driver = :selenium_chrome
+    when 'firefox_headless'
+      config.default_driver = :selenium_headless
+    when 'firefox'
+      config.default_driver = :selenium
+    end
+    
     config.app_host = CONFIG['url_default']
     config.default_max_wait_time = 10
+
 end
